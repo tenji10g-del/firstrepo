36702d06e237b620996714ded04cda8f73499a21
Tenji Sherpa <sherpatenjitoto3@gmail.com>
2025-05-03 21:05:21 +0545

    added some python code

---
+++ project1.py
@@ -0,0 +1,51 @@
+#for condition
+
+# mark=int(input("enter your mark"))
+
+# if(mark>=90):
+#     print("A+")
+# elif(mark<90 and mark>=80):
+#     print("A")
+# else:
+#     print("need to work ")
+
+#project2
+# number=int(input("enter number here:"))
+# result=number % 2
+#  #condition
+# if(result==0):
+#     print(" This number even")
+# else:
+#     print("this is odd number")
+
+#project3
+# a=input("enter first number:")
+# b=input("enter second number:")
+# c=input("enter third number:")
+# #condition
+# print("your greatest number ")
+# if(a>=b and a>=c):
+#     print(a)
+# elif(b>=a):
+#     print(b)
+# else:
+#     print(c)
+
+#project4
+
+num=int(input("enter number here:"))
+reminder=  num % 7 
+#condition 
+if(reminder==0):
+    print(num,"is multiple of 7")
+else:
+    print(num,"is not multiple of 7")
+
+
+
+
+
+
+
+
+
