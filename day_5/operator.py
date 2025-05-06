2fcf897775e5cb3126dfaefa835b110833e28bc9
Tenji Sherpa <sherpatenjitoto3@gmail.com>
2025-05-06 14:52:29 +0545

    Operators added

---
+++ day_5/operator.py
@@ -0,0 +1,51 @@
+#arithemetic_operators 
+# a=u=int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# sum=(a+b) #addition 
+# sub=(a-b ) #subtraction
+# multi=(a*b) #multiplication 
+# div=(a/b)  #division 
+# modu=(a%b) #modulas
+# expo=(a**b) #explnentiation 
+# floor=(a//b) #floor division 
+
+# print(sum)
+# print(multi)
+# print(sub)
+# print(modu)
+# print(expo)
+# print(div)
+# print(floor)
+
+
+#logical_operator 
+# x=10
+# y=3
+# print(x<5 and y>5)
+# print(x<15 or y>5)
+# print(not(x>y))
+
+
+#membership operators
+# var=["apple","banana","mango","litchy"]
+# print("apple in var:","apple" in var)
+# print("apple not in var:","apple" not in var)
+
+
+#python identity operators
+#case1
+p=[1,2,3]
+q=[1,2,3]
+print(p is q )
+print(id(p))
+print(id(q))
+#case2
+fruit="apple"
+plant="apple"
+print(fruit is plant)
+#id 
+print(id(fruit))
+print(id(plant))
+
+
+ 
