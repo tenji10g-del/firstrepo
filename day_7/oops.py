499abe27c788c9cae4c7c4c7a5c1293042b21652
Tenji Sherpa <sherpatenjitoto3@gmail.com>
2025-05-08 22:04:20 +0545

    updated upto 8th day

---
+++ day_7/oops.py
@@ -0,0 +1,98 @@
+
+# class car:
+#     def __init__(self, color,model):
+#         self.color=color#attribute 
+#         self.model=model#attribute
+#     def honk(self): #method 
+#         print("Beep Beep!")
+
+# #creating an object
+# my_car = car("red","toyota")
+# my_car.honk() #output:beep beep!
+
+       #encapsulation 
+# class Bankaccount:
+#     def __init__(self,balance):
+#         self.__balance=balance
+#     def deposit(self, amount):
+#         self.__balance += amount
+#     def get_balance(self):
+#         return self.__balance
+    
+# account= Bankaccount(100)
+# account.deposit(50)  
+# print(account.get_balance()) #output: 150 
+
+
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.__age = age  # Private attribute
+
+#     # Getter method
+#     def get_age(self):
+#         return self.__age
+
+#     # Setter method
+#     def set_age(self, age):
+#         self.__age = age 
+
+# stud = Student('DAWA', 12)
+
+# # Retrieving age using getter
+# print('NAME:', stud.name, stud.get_age())
+
+# # Changing age using setter
+# stud.set_age(16)
+
+# # Retrieving age using getter
+# print("NAME:", stud.name, stud.get_age())
+
+#practise_OOPs 
+
+# class coffeemachine:
+#     def __init__(self,brand, model, type):
+#         self.brand=brand
+#         self.model=model
+#         self.type=type 
+#     def display_info(self):
+#         return f"This is coffee machine with {self.brand} brand and model {self.model} and type {self.type}"
+
+# machine1=coffeemachine("tesla","A12","electric")
+# machine2=coffeemachine("ABC","B234","traditional")
+
+# print(machine1.display_info())
+# print(machine2.display_info())
+
+# class Employee:
+#     def __init__(self,name, age ,salary):
+#         self.name=name 
+#         self._age=age 
+#         self._salary=salary 
+#     def dispalyEmployee(self):
+#         print("Name:",self.name,"age:", self._age, "salary:",self._salary)
+# e1=Employee("RAM",24,10000)
+# print(e1.name)
+# print(e1._salary)
+# print(e1._age)
+# print(e1.dispalyEmployee)
+
+class Duck:
+    def sound(self):
+        return "Quack, Quack!"
+    
+class AnotherBird:
+    def sound(self):
+        return "I am similar to a duck "
+
+def makesound(duck):
+    print(duck.sound())  
+
+duck=Duck()
+anotherbird= AnotherBird() 
+
+makesound(duck)
+makesound(anotherbird)
+
+
+
--- practise.py
+++ practise.py
@@ -1,18 +1,24 @@
-#function demo
-def calc_avg(a,b,c): #function defination + parameter
-    sum=a+b+c        #work
-    avg=sum/3
-    print(avg)         
-    return avg
-    #some code 
-# calc_avg(2,3,3)      #function call
+# #function demo
+# def calc_avg(a,b,c): #function defination + parameter
+#     sum=a+b+c        #work
+#     avg=sum/3         
+#     return avg
+#     #some code 
+# # calc_avg(2,3,3)      #function call
 
-#for loop 
-x=int(input("enter number here:"))
-list1=[2,3,4,2,2,4,5,2,3]
-if  x in list1:
+# #for loop 
+# x=int(input("enter number here:"))
+# list1=[2,3,4,2,2,4,5,2,3]
+# if  x in list1:
+
+#     print("number found:")
+# else:
+#     print("number not found")
+
+for i in range(0,5):
+    if(i>5):
+        print(i*i)
+    else:
+        print("we do not know")
     
-    print("number found:")
-else:
-    print("number not found")
-#for loop
+    
